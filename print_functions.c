d3988b690e9f22c46972901138a5288a12d3bf0a
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-24 22:04:40 -0700

    My first commit

--- print_functions.c
+++ print_functions.c
@@ -2,13 +2,11 @@
 
 /**
  * print_char - prints character
- *
  * @ap: argument pointer
  * @params: the parameters struct
  *
  * Return: number chars printed
  */
-
 int print_char(va_list ap, params_t *params)
 {
 	char pad_char = ' ';
@@ -30,7 +28,6 @@ int print_char(va_list ap, params_t *params)
  *
  * Return: number chars printed
  */
-
 int print_int(va_list ap, params_t *params)
 {
 	long l;
@@ -46,13 +43,11 @@ int print_int(va_list ap, params_t *params)
 
 /**
  * print_string - prints string
- *
  * @ap: argument pointer
  * @params: the parameters struct
  *
  * Return: number chars printed
  */
-
 int print_string(va_list ap, params_t *params)
 {
 	char *str = va_arg(ap, char *), pad_char = ' ';
@@ -60,43 +55,41 @@ int print_string(va_list ap, params_t *params)
 
 	(void)params;
 	switch ((int)(!str))
-	case 1:
-		str = NULL_STRING;
+		case 1:
+			str = NULL_STRING;
 
-		j = pad = _strlen(str);
-		if (params->precision < pad)
-			j = pad = params->precision;
+	j = pad = _strlen(str);
+	if (params->precision < pad)
+		j = pad = params->precision;
 
-		if (params->minus_flag)
-		{
-			if (params->precision != UINT_MAX)
-				for (i = 0; i < pad; i++)
-					sum += _putchar(*str++);
-			else
-				sum += _puts(str);
-		}
-		while (j++ < params->width)
-			sum += _putchar(pad_char);
-		if (!params->minus_flag)
-		{
-			if (params->precision != UINT_MAX)
-				for (i = 0; i < pad; i++)
-					sum += _putchar(*str++);
-			else
-				sum += _puts(str);
-		}
-		return (sum);
+	if (params->minus_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (i = 0; i < pad; i++)
+				sum += _putchar(*str++);
+		else
+			sum += _puts(str);
+	}
+	while (j++ < params->width)
+		sum += _putchar(pad_char);
+	if (!params->minus_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (i = 0; i < pad; i++)
+				sum += _putchar(*str++);
+		else
+			sum += _puts(str);
+	}
+	return (sum);
 }
 
 /**
  * print_percent - prints string
- *
  * @ap: argument pointer
  * @params: the parameters struct
  *
  * Return: number chars printed
  */
-
 int print_percent(va_list ap, params_t *params)
 {
 	(void)ap;
@@ -106,13 +99,11 @@ int print_percent(va_list ap, params_t *params)
 
 /**
  * print_S - custom format specifier
- *
  * @ap: argument pointer
  * @params: the parameters struct
  *
  * Return: number chars printed
  */
-
 int print_S(va_list ap, params_t *params)
 {
 	char *str = va_arg(ap, char *);
