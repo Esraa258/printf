e9f3f78c63586839e869be2aa23fb8a020941eb6
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-24 22:01:46 -0700

    My first commit

--- params.c
+++ params.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * init_params - clears struct fields and resst buf
+ * init_params - clears struct fields and reset buf
  * @params: the parameters struct
  * @ap: the argument pointer
  *
@@ -18,7 +18,7 @@ void init_params(params_t *params, va_list ap)
 	params->minus_flag = 0;
 
 	params->width = 0;
-	params->precision = UNIT_MAX;
+	params->precision = UINT_MAX;
 
 	params->h_modifier = 0;
 	params->l_modifier = 0;
